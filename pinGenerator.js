6736145fe1de7e2a077e95ffe3c710a34c6bdfc9
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-08-20 18:13:53 +0100

    three different js file added

--- pinGenerator.js
+++ pinGenerator.js
@@ -1,59 +1,75 @@
 function pinGenerator() {
-    const pin = Math.round(Math.random() * 100000);
-    if ((pin / 10000) > 1 && (pin / 10000) <= 9) {
+    const pin = Math.round(Math.random() * 10000);
+    if ((pin / 1000) > 1 && (pin / 1000) <= 9) {
         return pin;
     }
     else {
-        console.log('hi');
         return pinGenerator();
     }
+    // OR
+    /* let Pin = Math.round(Math.random() * 10000);
+    let newPin = Pin + '';
+    if (newPin.length === 4) {
+        return Pin;
+    }
+    else {
+        console.log('pin incorrect');
+        return pinGenerator();
+    } */
 }
-
+// generatePin button
 document.getElementById('generatePin').addEventListener('click', function () {
     const pin = pinGenerator();
     const pinDisplay = document.getElementById('displayPin');
     pinDisplay.value = pin;
 })
 
+// clickedDigit calculator
 document.getElementById('calculatorBody').addEventListener('click', function (event) {
-    const clickedDigits = event.target.innerText;
+    const displaynumber = event.target.innerText;
+    const clickedDigit = document.getElementById('displayDigits');
+    const previousclickedDigit = clickedDigit.value;
+    // check NaN value
+    if (isNaN(displaynumber)) {
+        if (displaynumber == 'C') { clickedDigit.value = ''; }
 
-    const displayClickedDigits = document.getElementById('displayDigits');
-    const previousClickedDigits = displayClickedDigits.value;
-    const currentClickedDigits = previousClickedDigits + clickedDigits;
-
-    if (isNaN(clickedDigits)) {
-        if (clickedDigits == 'C') { displayClickedDigits.value = ''; }
-        else if (clickedDigits == '<') {
-            const splitCurrentClickedDigits = currentClickedDigits.split('');
-            splitCurrentClickedDigits.pop();
-            splitCurrentClickedDigits.pop();
-            const joinDigits = splitCurrentClickedDigits.join('');
-            displayClickedDigits.value = joinDigits;
+        else if (displaynumber == '<') {
+            // setting '<' functionality
+            let splitArray = previousclickedDigit.split('');
+            splitArray.pop();
+            let newNumber = splitArray.join('');
+            clickedDigit.value = newNumber;
         }
     }
     else {
-        displayClickedDigits.value = currentClickedDigits;
+        const newDisplayNumber = previousclickedDigit + displaynumber;
+        clickedDigit.value = newDisplayNumber;
     }
 })
-
+// submit button's functionality
 document.getElementById('submitBtn').addEventListener('click', function () {
-    const generatePinField = document.getElementById('displayPin');
-    const displayDigits = document.getElementById('displayDigits');
-
-    if (generatePinField.value == displayDigits.value) {
-        document.getElementById('successfulPin').style.display = 'block'
-        return;
+    // get value from generatePin field
+    const displayPinField = document.getElementById('displayPin');
+    const currentPin = displayPinField.value;
+    // get value from calculator field
+    const displayDigitsField = document.getElementById('displayDigits');
+    const currentDisplayDigit = displayDigitsField.value;
+    // comparing generatePin and calculator value
+    if (currentPin == currentDisplayDigit) {
+        document.getElementById('successfulPin').style.display = 'block';
     }
+
     else {
-        const startingTry = document.getElementById('tryLeft');
-        const startingTryString = startingTry.innerText;
-        const startingTryNumber = parseInt(startingTryString);
-        const tryLeft = startingTryNumber - 1;
-        if (tryLeft <= -1) {
-            return alert('access blocked');
-        }
-        startingTry.innerText = tryLeft;
-        document.getElementById('incorrectPin').style.display = 'block'
+        // counting false tries
+        let chancesLeft = document.getElementById('tryLeft');
+        let triesLeft = chancesLeft.innerText;
+        let triesLeftNumber = parseInt(triesLeft);
+        let chanceLeft = triesLeftNumber - 1;
+        if (chanceLeft <= -1) {
+            alert('access blocked');
+            return;
+        };
+        chancesLeft.innerText = chanceLeft; //stop counting at 0.
+        document.getElementById('incorrectPin').style.display = 'block';
     }
 })
