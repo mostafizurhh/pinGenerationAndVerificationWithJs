09c9cc1b085cebee97bb2ade9a88545990a6e795
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-08-20 06:56:30 +0100

    comment added on js file

--- pinGenerator.js
+++ pinGenerator.js
@@ -6,8 +6,7 @@ function pinGenerator() {
     else {
         return pinGenerator();
     }
-
-    // or
+    // OR
     /* let Pin = Math.round(Math.random() * 10000);
     let newPin = Pin + '';
     if (newPin.length === 4) {
@@ -18,21 +17,24 @@ function pinGenerator() {
         return pinGenerator();
     } */
 }
-
+// generatePin button
 document.getElementById('generatePin').addEventListener('click', function () {
     const pin = pinGenerator();
     const pinDisplay = document.getElementById('displayPin');
     pinDisplay.value = pin;
 })
 
+// clickedDigit calculator
 document.getElementById('calculatorBody').addEventListener('click', function (event) {
     const displaynumber = event.target.innerText;
     const clickedDigit = document.getElementById('displayDigits');
     const previousclickedDigit = clickedDigit.value;
-
+    // check NaN value
     if (isNaN(displaynumber)) {
         if (displaynumber == 'C') { clickedDigit.value = ''; }
+
         else if (displaynumber == '<') {
+            // setting '<' functionality
             let splitArray = previousclickedDigit.split('');
             splitArray.pop();
             let newNumber = splitArray.join('');
@@ -44,18 +46,21 @@ document.getElementById('calculatorBody').addEventListener('click', function (ev
         clickedDigit.value = newDisplayNumber;
     }
 })
-
+// submit button's functionality
 document.getElementById('submitBtn').addEventListener('click', function () {
+    // get value from generatePin field
     const displayPinField = document.getElementById('displayPin');
     const currentPin = displayPinField.value;
-
+    // get value from calculator field
     const displayDigitsField = document.getElementById('displayDigits');
     const currentDisplayDigit = displayDigitsField.value;
-
+    // comparing generatePin and calculator value
     if (currentPin == currentDisplayDigit) {
         document.getElementById('successfulPin').style.display = 'block';
     }
+
     else {
+        // counting false tries
         let chancesLeft = document.getElementById('tryLeft');
         let triesLeft = chancesLeft.innerText;
         let triesLeftNumber = parseInt(triesLeft);
@@ -64,7 +69,7 @@ document.getElementById('submitBtn').addEventListener('click', function () {
             alert('access blocked');
             return;
         };
-        chancesLeft.innerText = chanceLeft;
+        chancesLeft.innerText = chanceLeft; //stop counting at 0.
         document.getElementById('incorrectPin').style.display = 'block';
     }
 })
